1a890780a844ab22bbc85eb379b5d4df5b714636
Neo <c.min.tseng@gmail.ocm>
2024-10-09 18:34:56 +0800

    split to demo app & xposed(Lsposed) app

--- app/src/main/java/com/hyb/hookdemo/MainActivity.kt
+++ app/src/main/java/com/hyb/hookdemo/MainActivity.kt
@@ -1,32 +1,20 @@
-package com.hyb.hookdemo;
+package com.hyb.hookdemo
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-/**
- * @author Mr.HuaYunBin
- */
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, toastMsg(), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    private String toastMsg() {
-        return "我未被劫持";
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
