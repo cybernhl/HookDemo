0ced173d6b6c24378ccd2ffb5948a9e169f4d96b
Neo <c.min.tseng@gmail.ocm>
2024-10-11 17:09:17 +0800

    add hook okhttp - Ref :  https://github.com/Harlber/HookOkHttp

--- app/src/main/java/com/hyb/hookdemo/HookUtils.java
+++ app/src/main/java/com/hyb/hookdemo/HookUtils.java
@@ -6,19 +6,39 @@ import android.app.Service;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
 import timber.log.Timber;
+
+import android.annotation.SuppressLint;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+
+
+import okhttp3.internal.http.RealInterceptorChain;
 
 /**
  * @author Mr.HuaYunBin
  */
 public class HookUtils implements IXposedHookLoadPackage , IXposedHookZygoteInit {
+    private static final String TAG = "HookUtils";
     private static final boolean ENABLE_LOG_HOOKER = true;
+    private static final byte[] lock = new byte[1];
+    private Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+    private boolean classInDexFound = false;
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
@@ -39,6 +59,7 @@ public class HookUtils implements IXposedHookLoadPackage , IXposedHookZygoteInit
 //        hookXpHookAllConstructors();
     }
 
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         Timber.plant(new Timber.DebugTree());//FIXME init for Hook module
@@ -51,15 +72,81 @@ public class HookUtils implements IXposedHookLoadPackage , IXposedHookZygoteInit
                 @Override
                 protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
                     super.beforeHookedMethod(param);
+                    Log.e(TAG, "Show me beforeHookedMethod"  );
                 }
 
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     param.setResult("你被劫持啦");
+                    Log.e(TAG, "Show me afterHookedMethod"  );
+                }
+            });
+            //hookOriginNewCall(lpparam);
+            XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    final ClassLoader cl = ((Context) param.args[0]).getClassLoader();
+
+                    Class<?> aClass = cl.loadClass("okhttp3.OkHttpClient");
+                    Class<?> requestClass = cl.loadClass("okhttp3.Request");
+                    findAndHookMethod(aClass, "newCall", requestClass, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            super.beforeHookedMethod(param);
+                            try {
+                                //okhttp3.Request cannot be cast to okhttp3.Request
+                                String str = toJson(param.args[0]);
+                                Log.i(TAG, "request json string " + str);
+                                //Request request = gson.fromJson(str, Request.class);
+                                //set result 修改请求参数
+                            } catch (Throwable e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            Log.i(TAG, "OkHttpClient afterHookedMethod package name = " + lpparam.packageName);
+                        }
+                    });
                 }
             });
         }
     }
+
+
+    private void hookOriginNewCall(final LoadPackageParam lp) {
+        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                final ClassLoader cl = ((Context) param.args[0]).getClassLoader();
+
+                Class<?> aClass = cl.loadClass("okhttp3.OkHttpClient");
+                Class<?> requestClass = cl.loadClass("okhttp3.Request");
+                findAndHookMethod(aClass, "newCall", requestClass, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        try {
+                            //okhttp3.Request cannot be cast to okhttp3.Request
+                            String str = toJson(param.args[0]);
+                            Log.i(TAG, "request json string " + str);
+                            //Request request = gson.fromJson(str, Request.class);
+                            //set result 修改请求参数
+                        } catch (Throwable e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        Log.i(TAG, "OkHttpClient afterHookedMethod package name = " + lp.packageName);
+                    }
+                });
+            }
+        });
+    }
+
 
     private void hookGetPackageManager(XC_LoadPackage.LoadPackageParam lpparam) {
         String target_hook_applicationId = "com.hyb.hookdemo.app";
@@ -73,12 +160,12 @@ public class HookUtils implements IXposedHookLoadPackage , IXposedHookZygoteInit
                 @Override
                 protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
                     super.beforeHookedMethod(param);
-
+                    Log.e(TAG,"Show me Yooo");
                 }
 
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-
+                    Log.e(TAG,"Show me YoooKKK");
                     param.setResult("你被劫持啦");
                 }
             });
@@ -166,5 +253,109 @@ public class HookUtils implements IXposedHookLoadPackage , IXposedHookZygoteInit
         } catch (Exception e) {
             XposedBridge.log(e);
         }
+    }
+
+    private final OnFindDexListener findDexListener = new OnFindDexListener() {
+        @Override
+        public void onFind(ClassLoader classLoader) {
+            if (classInDexFound) {
+                Log.i(TAG, "found in hide dex");
+                return;
+            }
+
+            realHookLastInterceptor(classLoader);
+        }
+    };
+
+    //java.lang.ClassNotFoundException: Didn't find class "okhttp3.Interceptor.Chain" on path: DexPathList
+    private void hookLastInterceptor(final LoadPackageParam lp) {
+        Log.i(TAG, "start hookLastInterceptor:" + lp.packageName);
+        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                final ClassLoader cl = ((Context) param.args[0]).getClassLoader();
+
+                try {
+                    cl.loadClass("okhttp3.internal.http.CallServerInterceptor");
+                    cl.loadClass("okhttp3.Interceptor.Chain");
+                    classInDexFound = true;
+                    realHookLastInterceptor(cl);
+                } catch (Throwable e) {
+                    if (e instanceof ClassNotFoundException) {
+                        findHideDex(findDexListener);
+                    } else {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+    }
+
+
+    private void realHookLastInterceptor(final ClassLoader cl) {
+        try {
+            Class<?> aClass = cl.loadClass("okhttp3.internal.http.CallServerInterceptor");
+            Class<?> chainClass = cl.loadClass("okhttp3.Interceptor.Chain");
+            findAndHookMethod(aClass, "intercept", chainClass, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                    classInDexFound = true;
+                    try {
+                        //RealInterceptorChain realChain = (RealInterceptorChain) chain;
+                        String str = gson.toJson(param.args[0]);
+                        Log.i(TAG, "RealInterceptorChain json string " + str);
+                        RealInterceptorChain realChain = gson.fromJson(str, RealInterceptorChain.class);
+                        if (realChain != null) {
+                            String requestStr = toJson(realChain.request());
+                            Log.i(TAG, "RealInterceptorChain request json string " + requestStr);
+                        }
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (Throwable e) {
+            //ignore
+        }
+    }
+
+    /**
+     * 查找其他dex中class
+     */
+    private void findHideDex(final OnFindDexListener listener) {
+        XposedBridge.hookAllMethods(ContextWrapper.class, "attachBaseContext", new XC_MethodHook() {
+
+            public void beforeHookedMethod(MethodHookParam param) {
+                ClassLoader classLoader = ((Context) param.args[0]).getClassLoader();
+                if (classLoader == null) return;
+                if (listener != null) listener.onFind(classLoader);
+            }
+        });
+        XposedBridge.hookAllConstructors(ClassLoader.class, new XC_MethodHook() {
+            public void beforeHookedMethod(MethodHookParam param) {
+                ClassLoader classLoader = (ClassLoader) param.args[0];
+                if (classLoader == null) return;
+                if (listener != null) listener.onFind(classLoader);
+            }
+        });
+    }
+
+
+    private String toJson(Object object) {
+        synchronized (lock) {
+            return gson.toJson(object);
+        }
+    }
+
+    private <T> T fromJson(String string, Class<T> classOfT) {
+        synchronized (lock) {
+            return gson.fromJson(string, classOfT);
+        }
+    }
+
+    public interface OnFindDexListener {
+        void onFind(ClassLoader classLoader);
     }
 }
---
+++ app/src/main/java/com/hyb/hookdemo/OkHttpHooker.java
@@ -0,0 +1,68 @@
+package com.hyb.hookdemo;
+
+import static com.hyb.hookdemo.OkCompat.Cls_OkHttpClient$Builder;
+import static com.hyb.hookdemo.OkCompat.F_Builder_interceptors;
+import static com.hyb.hookdemo.OkCompat.M_Builder_build;
+
+import android.util.Log;
+
+import java.util.List;
+
+import de.robv.android.xposed.XC_MethodHook;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
+//Ref : https://blog.csdn.net/moziqi123/article/details/109204801
+//Ref : https://github.com/siyujie/OkHttpLoggerInterceptor-hook/tree/master
+//Ref : https://github.com/Harlber/HookOkHttp
+//Ref : https://github.com/Harlber/HookOkHttp/blob/master/app/src/main/java/just/hook/ok/OkHttpHook.java
+//Ref : https://github.com/Harlber/HookOkHttp/blob/master/app/src/main/java/just/hook/ok/OkCompat.java
+//Ref : https://blog.51cto.com/u_16993871/11908417
+
+/**
+ * createTime: 2020/4/16.13:56
+ * updateTime: 2020/4/16.13:56
+ * author: singleMan.
+ * desc:
+ */
+public class OkHttpHooker {
+
+    public static final String TAG = "OkHttp";
+
+
+    /**
+     *
+     * @param classLoader
+     */
+    public static void attach(final ClassLoader classLoader){
+
+        Class OkHttpClient_BuilderClass = null;
+        try {
+            OkHttpClient_BuilderClass = Class.forName(Cls_OkHttpClient$Builder, true, classLoader);
+            if(null == OkHttpClient_BuilderClass){
+                return;
+            }
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+            findAndHookMethod(OkHttpClient_BuilderClass, M_Builder_build, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                    List interceptors = (List) getObjectField(param.thisObject, F_Builder_interceptors);
+                    //添加自己的过滤器
+//                    interceptors.add(new MyInterceptor.Builder()
+//                            .addHeader("customHeaderKey","customHeaderValue")
+//                            .addQueryParam("customQueryKey","customQueryValue")
+//                            .build(classLoader));
+
+                    interceptors.add(new MyInterceptor.Builder().build(classLoader));
+                    interceptors.add(logging);
+                }
+            });
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Log.d(TAG,"没有发现OkHttp相关，可能未使用 or 被混淆");
+        }
+    }
+}
